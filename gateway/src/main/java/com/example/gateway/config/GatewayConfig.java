ef943460b5eae10bd7bc41cbf097b59dc7773281
ThuanNgcodelor <nguyentrungthuan417@gmail.com>
2025-06-17 13:41:03 +0700

    #1 shopee

---
+++ gateway/src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -0,0 +1,32 @@
+package com.example.gateway.config;
+
+import com.example.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    private final JwtAuthenticationFilter filter;
+
+    public GatewayConfig(JwtAuthenticationFilter filter) {
+        this.filter = filter;
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/user/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://user-service"))
+
+                .route("stock-domains", r -> r.path("/stock-service/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://stock-service"))
+
+                .route("auth-service", r -> r.path("/v1/auth/**")
+                        .uri("lb://auth-service"))
+                .build();
+    }
+}
